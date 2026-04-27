28f9b62c9c3d76b60bfad1a6f2c7a3e6737dd81a
KoYunSeok <a01021719359@gmail.com>
2026-04-27 16:21:41 +0900

    미반영 파일 추가 커밋

---
+++ front-end/src/lib/use-theme.ts
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function useTheme() {
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    const isDark = localStorage.getItem("theme") === "dark";
+    setDark(isDark);
+    if (isDark) document.documentElement.setAttribute("data-theme", "dark");
+  }, []);
+
+  function toggle() {
+    const next = !dark;
+    setDark(next);
+    if (next) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.removeAttribute("data-theme");
+      localStorage.setItem("theme", "light");
+    }
+  }
+
+  return { dark, toggle };
+}
